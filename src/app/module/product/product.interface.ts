832b885f732618ee4136c294009a231a66e59b46
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-06-04 20:41:03 +0600

    produc typescript type define

---
+++ src/app/module/product/product.interface.ts
@@ -0,0 +1,22 @@
+
+
+export type TVariant = {
+    type: string,
+    value: string,
+}
+
+export type TInventory = {
+    quantity: number,
+    inStock: boolean,
+}
+
+
+export type TProduct = {
+    name: string,
+    description: string,
+    price: number,
+    category: string,
+    tags: string[],
+    variants: TVariant[],
+    inventory: TInventory,
+}
