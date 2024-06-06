e26abe2edbdb37c154072510c0e75bbb6b954ec2
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-06-06 22:00:52 +0600

    readme file

---
+++ README.md
@@ -0,0 +1,26 @@
+### E-Commerce Backend API
+
+## Installation
+
+First, clone this repository:
+
+# Clone this repository
+
+git clone https://github.com/rhshuvo44/ecommerce-backend.git
+cd ecommerce-backend
+
+# Install dependencies
+
+npm install
+
+# Copy the example .env file
+
+cp .env.example .env
+
+# Run the app
+
+npm run start:dev
+
+# Open http://localhost:5000 in your browser
+
+open http://localhost:5000
--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,18 @@ const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
-
+// app route 
 app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter);
-
+// root route 
 app.get('/', (req: Request, res: Response) => {
   res.send('Asserment 2 E-commerce Backend Server');
 });
-
+// Not Found Route Handler
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found"
+  });
+});
 export default app;
