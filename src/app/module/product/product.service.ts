c572951115f2442b65ca7614b002a28511a5d619
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2024-06-06 20:45:05 +0600

    get api with search

--- src/app/module/product/product.service.ts
+++ src/app/module/product/product.service.ts
@@ -8,15 +8,9 @@ const createProduct = async (product: TProduct) => {
 };
 //get all products from the database
 const getAllProduct = async (searchTerm: string | undefined) => {
-  if (searchTerm) {
-    const result = await Product.findOne({ name: searchTerm });
 
-    // const result = await Product.find({ $in: [{ $name: searchTerm }] });
-    // // Filter products based on search term
-    // // result = Product.filter(product =>
-    // //   product.name.toLowerCase().includes(searchTerm.toLowerCase())
-    // // );
-    // result = Product.findIndex(product => console.log(product))
+  if (searchTerm) {
+    const result = await Product.find({ name: { $regex: searchTerm, $options: 'i' } });
     return {
       result,
       message: `Products matching search term '${searchTerm}' fetched successfully!`,
